9bcda1112bf4af44b28fc2c70f74259e451f028d
wufs <wufs@jxgtmbjt.com>
2018-12-14 17:11:55 +0800

    <A> kotlin 提交git代码

---
+++ app/src/main/java/com/wufs/android_customtoast/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.wufs.android_customtoast
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initView()
+    }
+
+    fun initView() {
+        button.setText("按钮1")
+        button2.setText("按钮2")
+        button3.setText("按钮3")
+        button4.setText("按钮4")
+        button.setOnClickListener(View.OnClickListener {
+            tv_first.setText("我点击了第一个按钮")
+        })
+
+    }
+}
